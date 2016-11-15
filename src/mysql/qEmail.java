95acd30ee50b8f2162fa75b188054fab392b1123
Chinmay Nivsarkar <mit.chinmayn@gmail.com>
2016-11-16 02:55:27 +0530

    Added files

--- src/mysql/qEmail.java
+++ src/mysql/qEmail.java
@@ -5,15 +5,64 @@
  */
 package mysql;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author chinmay
  */
 public class qEmail {
+    static ResultSet rs;
+    static MyDBConnection a= new MyDBConnection();
+    static ArrayList<String[]> AL,AL2;
+    static String sql;
+    
+    
     public static void main(){
+       
+        try {
+            if(!a.status)
+                a.init();
+            java.sql.Statement stmt = a.myConnection.createStatement();
+            
+            sql = "SELECT sid from el where cnt>=3";
+            qEmail.rs = stmt.executeQuery(sql);
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(qEmail.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        AL=new ArrayList<String[]>();  
+        AL2=new ArrayList<String[]>(); 
+      try{
+        while(rs.next()){
+      
+              String[] row={
+                rs.getString("sid"),rs.getString("cnt")
+          
+            };
+            
+            AL.add(row);
+      
+         }   
+      }
+      catch(Exception e){
+          System.out.println("Exception in ArrayList");
+            }
+        
+        sql = "SELECT sid from el where cnt>=3";
+            qEmail.rs = stmt.executeQuery(sql);
+        
+        //Old code
         System.out.println("Entry");
-        GmailClient g = new GmailClient();
-        g.sendGmail(g.senderUserName,"chinmayn96@yahoo.co.in","test","ABCDEFGH");
-        System.out.println("Done");
+            GmailClient g = new GmailClient();
+            g.sendGmail(g.senderUserName,"chinmayn96@yahoo.co.in","test","ABCDEFGH");
+            System.out.println("Done");
     }
+    
 }
